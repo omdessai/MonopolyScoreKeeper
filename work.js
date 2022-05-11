29780f6211aa442c0db16ec8b51f04883b0cfe7e
omdessai <omndessai@gmail.com>
2022-05-11 19:59:39 -0400

    adding cors

--- work.js
+++ work.js
@@ -30,6 +30,10 @@ function CreditOrDebit(playerNum, amount){
 }
 
 var server = http.createServer(function (req, res) {   
+    res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	res.setHeader('Access-Control-Allow-Headers', '*');
     //console.log(req);
     const queryObject = url.parse(req.url, true).query;
     //console.log(queryObject);
